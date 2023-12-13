0eefc6a99e0c623bcb1465f777419087306bf2ff
Gabriel Assis <biel.v.assis@gmail.com>
2023-12-13 00:59:53 -0300

    edit functionality added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,34 @@
 import axios from "axios";
 import { url } from "../config.js";
-import { Card, List, Button } from "antd";
-import { DeleteFilled } from "@ant-design/icons";
+import { Card, List, Button, Space, Modal, Form, Input } from "antd";
+import { DeleteFilled, EditFilled } from "@ant-design/icons";
+import { useState } from "react";
 
 const TodoList = (props) => {
-  const onClickHandler = async (id) => {
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = (todo) => {
+    form.setFieldValue("id", todo.id);
+    form.setFieldValue("title", todo.title);
+    form.setFieldValue("description", todo.description);
+    setIsModalOpen(true);
+  };
+
+  const onFinish = async (todo) => {
+    const response = await axios.put(url + "/todo", todo);
+    const newTodo = response.data;
+    props.setTodos((todos) => {
+      const foundIndex = todos.findIndex((todo) => todo.id == newTodo.id);
+      todos[foundIndex] = newTodo;
+      return todos;
+    });
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const onClickDeleteHandler = async (id) => {
     await axios.delete(url + "/todo/" + id);
     props.setTodos((todos) => {
       return todos.filter((todo) => todo.id !== id);
@@ -12,24 +36,62 @@ const TodoList = (props) => {
   };
 
   return (
-    <List>
-      {props.todos.map((todo) => {
-        return (
-          <Card key={todo.id} title={todo.title}>
-            {todo.description}
-            <Button
-              style={{ float: "right" }}
-              type="primary"
-              danger
-              icon={<DeleteFilled />}
-              onClick={() => {
-                onClickHandler(todo.id);
-              }}
-            ></Button>
-          </Card>
-        );
-      })}
-    </List>
+    <div>
+      <List>
+        {props.todos.map((todo) => {
+          return (
+            <Card key={todo.id} title={todo.title}>
+              {todo.description}
+              <Button.Group style={{ float: "right" }}>
+                <Space>
+                  <Button
+                    type="primary"
+                    icon={<EditFilled />}
+                    onClick={() => {
+                      showModal(todo);
+                    }}
+                  ></Button>
+                  <Button
+                    type="primary"
+                    danger
+                    icon={<DeleteFilled />}
+                    onClick={() => {
+                      onClickDeleteHandler(todo.id);
+                    }}
+                  ></Button>
+                </Space>
+              </Button.Group>
+            </Card>
+          );
+        })}
+      </List>
+      <Modal
+        title="Editar"
+        open={isModalOpen}
+        onOk={form.submit}
+        onCancel={handleCancel}
+      >
+        <Form form={form} name="editTodo" onFinish={onFinish}>
+          <Form.Item name="id" hidden></Form.Item>
+          <Form.Item
+            label="Título"
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: "Insira um título!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Descrição" name="description">
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
   );
 };
 
